a4f46878646d02e9de1f0d67d1de9379e90bc71f
Dominic Brant <dombrant@gmail.com>
2019-04-08 14:46:59 -0700

    removed unecessary return statement

--- get.js
+++ get.js
@@ -1,18 +1,17 @@
 // Code from https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
 
-const http = require('http');
-const https = require('https');
+const http = require("http");
+const https = require("https");
 
-const get = url => {
-  return new Promise((resolve, reject) => {
-    const handler = url.startsWith('https') ? https : http;
-    const request = handler.get(url, response => {
-      let body = '';
-      response.on('data', chunk => (body += chunk));
-      response.on('end', () => resolve(JSON.parse(body)));
+const get = url =>
+    new Promise((resolve, reject) => {
+        const handler = url.startsWith("https") ? https : http;
+        const request = handler.get(url, response => {
+            let body = "";
+            response.on("data", chunk => (body += chunk));
+            response.on("end", () => resolve(JSON.parse(body)));
+        });
+        request.on("error", error => reject(`Error: ${error}`));
     });
-    request.on('error', error => reject(`Error: ${error}`));
-  });
-};
 
 module.exports = get;
